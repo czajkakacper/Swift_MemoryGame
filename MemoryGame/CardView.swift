bac00e3b6a9eabda86e3197066f7053649403473
czajkakacper <czajkaakacper@gmail.com>
2024-10-28 20:13:52 +0100

    Update ContentView.swift

--- MemoryGame/CardView.swift
+++ MemoryGame/CardView.swift
@@ -2,42 +2,42 @@
 //  CardView.swift
 //  MemoryGame
 //
-//  Created by student on 22/10/2024.
+//  Created by student on 29/10/2024.
 //
 
 import SwiftUI
 
 struct CardView: View {
-    @State var isReverse: Bool = false
-    var emoji: String
-    var body: some View {
-        ZStack{
-            Group{
-                RoundedRectangle(cornerRadius: 12)
-                    .stroke(Color.blue, lineWidth: 6)
-                if isReverse {
-                    Text(emoji)
-                        .font(.largeTitle)
-                        .opacity(isReverse ? 1 : 0)
-                } else {
-                    Text("")
-                }
-                RoundedRectangle(cornerRadius: 12)
-                    .fill(Color.blue)
-                    .opacity(isReverse ? 0 : 1)
+	@State var isReverse: Bool = false
+	var emoji: String
+	var body: some View {
+		ZStack{
+			Group{
+				RoundedRectangle(cornerRadius: 12)
+					.stroke(Color.blue, lineWidth: 6)
+				if isReverse {
+					Text(emoji)
+						.font(.largeTitle)
+						.opacity(isReverse ? 1 : 0)
+				} else {
+					Text("")
+				}
+				RoundedRectangle(cornerRadius: 12)
+					.fill(Color.blue)
+					.opacity(isReverse ? 0 : 1)
 
-            }
-        }
-        .onTapGesture {
-            withAnimation {
-                isReverse.toggle()
-            }
-        }
-        .frame(width: 100, height: 100)
-        .padding(.top, 10)
-    }
+			}
+		}
+		.onTapGesture {
+			withAnimation {
+				isReverse.toggle()
+			}
+		}
+		.frame(width: 100, height: 100)
+		.padding(.top, 10)
+	}
 }
 
 #Preview {
-    CardView(emoji: "👻")
+	CardView(emoji: "👻")
 }
--- MemoryGame/ContentView.swift
+++ MemoryGame/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    var tab = ["😊", "😅", "🚀", "😂", "🐐", "🚀", "😊", "🐐", "😂", "😅"]
+    @State var cards_num = 2
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ScrollView{
+                cardDisplay
+            }
+            HStack {
+                adjustCardNumber(by: -2, symbol: "minus.square")
+                    .disabled(cards_num <= 2)
+                    .opacity(cards_num <= 2 ? 0.2 : 1)
+                Spacer()
+                adjustCardNumber(by: 2, symbol: "plus.square")
+                    .disabled(cards_num >= tab.count)
+                    .opacity(cards_num >= tab.count ? 0.2 : 1)
+            }
+            .padding()
+            
         }
         .padding()
+    }
+    
+    func adjustCardNumber(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            withAnimation {
+                cards_num += offset
+            }
+        }) {
+            Image(systemName: symbol)
+                .font(.largeTitle)
+                .foregroundColor(.blue)
+        }
+    }
+    
+    var cardDisplay: some View {
+        LazyVGrid(columns:  [GridItem(.adaptive(minimum: 120))]){
+            ForEach(0..<cards_num, id: \.self){
+                i in
+                CardView(emoji: tab[i])
+            }
+        }
+        .foregroundColor(.blue)
     }
 }
 
